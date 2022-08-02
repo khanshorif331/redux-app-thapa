c030bd6e99b259fef9fda0f5619bcb60a52949ef
khanshorif331 <khanshorif331@gmail.com>
2022-08-02 19:29:39 +0600

    conbining the reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import changeTheNumber from './updown'
+
+import { combineReducers } from 'redux'
+
+const rootReducer = combineReducers({
+	// all the reducers will be here in this object
+	changeTheNumber,
+})
--- src/reducers/updown.js
+++ src/reducers/updown.js
@@ -9,3 +9,5 @@ const changeTheNumber = (state = initialState, action) => {
 			return state
 	}
 }
+
+export default changeTheNumber
