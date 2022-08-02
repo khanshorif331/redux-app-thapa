4fe1466d5445e00c6c9b9172cea6e02ced89bacf
khanshorif331 <khanshorif331@gmail.com>
2022-08-02 19:31:36 +0600

    root reducer created successfully

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,5 +4,8 @@ import { combineReducers } from 'redux'
 
 const rootReducer = combineReducers({
 	// all the reducers will be here in this object
+	// changeTheNumber : changeTheNumber, ; same thing ,,we can write any
 	changeTheNumber,
 })
+
+export default rootReducer
