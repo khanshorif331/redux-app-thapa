c4995fc98aa3d783334907b4ae274a08c18e6f1b
khanshorif331 <khanshorif331@gmail.com>
2022-08-04 20:13:02 +0600

    updated

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import store from './store'
 import { Provider } from 'react-redux'
-
+//
 store.subscribe(() => {
 	console.log(store.getState())
 })
